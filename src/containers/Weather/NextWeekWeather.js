1dc32c388c7610fdb9365e2226f6928d8671b241
Dawid Holisz <dawid.holisz@gmail.com>
2019-08-22 09:31:06 +0200

    Feat-5 add proptypes for NextWeekWeather

--- src/containers/Weather/NextWeekWeather.js
+++ src/containers/Weather/NextWeekWeather.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import withWeather from '../../hocs/withWeather'
 import DayWeather from './DayWeather'
+import PropTypes from 'prop-types'
 
 class NextWeekWeather extends Component {
   forecast = this.props.weather.forecast.forecastday.slice(1)
@@ -24,6 +25,24 @@ class NextWeekWeather extends Component {
       </div>
     )
   }
+}
+NextWeekWeather.propTypes = {
+  weather: PropTypes.shape({
+    forecast: PropTypes.shape({
+      forecastday: PropTypes.arrayOf(
+        PropTypes.shape({
+          date: PropTypes.string.isRequired,
+          day: PropTypes.shape({
+            maxtemp_c: PropTypes.number.isRequired,
+            mintemp_c: PropTypes.number.isRequired,
+            condition: PropTypes.shape({
+              icon: PropTypes.string,
+            }).isRequired,
+          }).isRequired,
+        }),
+      ),
+    }).isRequired,
+  }).isRequired,
 }
 
 export default withWeather(NextWeekWeather)
